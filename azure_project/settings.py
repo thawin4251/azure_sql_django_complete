ebea8bb29c242ef7d5ad83e1051b307a69e67aed
thawin4251 <thawin4251@gmail.com>
2026-02-05 15:55:54 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -78,15 +78,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'azure_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
      'default': {
          'ENGINE': 'mssql',
@@ -100,6 +91,7 @@ DATABASES = {
          },
     },
  }
+
 
 
 import urllib.parse
