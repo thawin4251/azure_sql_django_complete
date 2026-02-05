8055e42c62f7aa76c7d06a9cfbd45a636d9c7fe7
thawin4251 <thawin4251@gmail.com>
2026-02-05 17:07:07 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST': 'super0978.database.windows.net',
          'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
+            'driver': 'ODBC Driver 17 for SQL Server',
          },
     },
  }
